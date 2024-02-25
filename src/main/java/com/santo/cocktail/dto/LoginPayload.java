db55760ad03d3a87d3895e16b46a7cfcc8227a5e
Yassine ZOUAZOU <yassine.zouazou@ensiie.fr>
2024-02-25 22:52:18 +0100

    cleanup

--- src/main/java/com/santo/cocktail/dto/LoginPayload.java
+++
@@ -1,5 +0,0 @@
-package com.santo.cocktail.dto;
-
-public record LoginPayload(String localId, String email, String displayName, String idToken, boolean registered,
-                           String refreshToken, String expiresIn) {
-}
